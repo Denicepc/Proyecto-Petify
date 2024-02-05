6bc23dfe109d5f8bdf77bf4ee00604e4326409e8
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-02-05 14:07:37 +0100

    c

--- frontend/src/app/components/mis-compras/mis-compras.component.ts
+++ frontend/src/app/components/mis-compras/mis-compras.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 import { MisCompras } from 'src/app/models/mis-compras';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { MisComprasService } from 'src/app/services/mis-compras.service';
@@ -9,26 +9,28 @@ import { MisComprasService } from 'src/app/services/mis-compras.service';
   styleUrls: ['./mis-compras.component.css']
 })
 export class MisComprasComponent {
-  
   public totalCompras: number = 0; //varible donde almacenaremos el total
   public misCompras: MisCompras[] = [];
   public emailUsuario: string = "null";
 
-  constructor(public misComprasService: MisComprasService,
+
+  constructor(
+    public misComprasService: MisComprasService,
     public usuarioService: UsuarioService){}
+
 
   ngOnInit(): void {
     this.emailUsuario = this.usuarioService.obtenerEmailUsuarioLogeado();
-    
-    this.misComprasService.misComprasSeleccionadas$.subscribe( 
+
+    this.misComprasService.misComprasSeleccionadas$.subscribe(
       compras => {
         this.misCompras = compras;
         //calculas el totoal de la comrpa
         this.totalCompras = 0;
-        
+
         for (let compra of compras) {
           this.totalCompras += Number(compra.total);
-        }        
+        }
         console.log("MIS COMPRAS: ", compras);
       },
       error => console.error('Error al obtener compras', error)
@@ -38,11 +40,11 @@ export class MisComprasComponent {
   //metodo calcularTotal
   calcularTotalCompras(): void {
     this.totalCompras = 0; //contador
-    this.misCompras.forEach(compra => {  
+    this.misCompras.forEach(compra => {
       this.totalCompras += Number(compra.total);
     });
   }
 
-  
+
 
 }
