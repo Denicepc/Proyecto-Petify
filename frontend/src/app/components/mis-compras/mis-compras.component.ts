ff90a6940405472fea974b1ce7d244dc78a633f7
marioolopez <mariolopezaparicio2002@gmail.com>
2024-03-13 19:59:00 +0100

    seguir

--- frontend/src/app/components/mis-compras/mis-compras.component.ts
+++ frontend/src/app/components/mis-compras/mis-compras.component.ts
@@ -1,7 +1,8 @@
 import { Component,  OnInit } from '@angular/core';
 import { MisCompras } from 'src/app/models/mis-compras';
 import { UsuarioService } from 'src/app/services/usuario.service';
-import { MisComprasService } from 'src/app/services/mis-compras.service';
+import { MisComprasService } from 'src/app/services/mis-compras.service'; 
+//ESTE ES EL COMPONENTE DE MIS COMPRAS, CUANDO QUIERES VER TODAS LAS COMPRAS DEL USUARIO
 
 @Component({
   selector: 'app-mis-compras',
@@ -9,23 +10,22 @@ import { MisComprasService } from 'src/app/services/mis-compras.service';
   styleUrls: ['./mis-compras.component.css']
 })
 export class MisComprasComponent {
-  public totalCompras: number = 0; //varible donde almacenaremos el total
-  public misCompras: MisCompras[] = [];
-  public emailUsuario: string = "null";
-
+  public totalCompras: number = 0; //variable donde almacenaremos el total
+  public misCompras: MisCompras[] = []; //almacenas todos los productos
+  public emailUsuario: string = "null"; //donde almacenas el usuario vacio
 
   constructor(
-    public misComprasService: MisComprasService,
+    public misComprasService: MisComprasService, //almacenas todas las compras
     public usuarioService: UsuarioService){}
 
-
-  ngOnInit(): void {
-    this.emailUsuario = this.usuarioService.obtenerEmailUsuarioLogeado();
+  ngOnInit(): void { //EJRCICIO 27
+    
+    this.emailUsuario = this.usuarioService.obtenerEmailUsuarioLogeado(); //obtienes el email del usuario logeado
 
     this.misComprasService.misComprasSeleccionadas$.subscribe(
       compras => {
         this.misCompras = compras;
-        //calculas el totoal de la comrpa
+        //calculas el total de la compra
         this.totalCompras = 0;
 
         for (let compra of compras) {
@@ -40,11 +40,9 @@ export class MisComprasComponent {
   //metodo calcularTotal
   calcularTotalCompras(): void {
     this.totalCompras = 0; //contador
-    this.misCompras.forEach(compra => {
-      this.totalCompras += Number(compra.total);
+    this.misCompras.forEach(compra => { //recorres todas las compras 
+      this.totalCompras += Number(compra.total); //sumas el total de todas las compras
     });
   }
-
-
 
 }
