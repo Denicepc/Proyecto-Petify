a12908e06f684d078c0340c703cfcac97bfce9f0
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-03-18 12:18:06 +0100

    c

--- frontend/src/app/components/mis-compras/mis-compras.component.ts
+++ frontend/src/app/components/mis-compras/mis-compras.component.ts
@@ -1,7 +1,7 @@
 import { Component,  OnInit } from '@angular/core';
 import { MisCompras } from 'src/app/models/mis-compras';
 import { UsuarioService } from 'src/app/services/usuario.service';
-import { MisComprasService } from 'src/app/services/mis-compras.service'; 
+import { MisComprasService } from 'src/app/services/mis-compras.service';
 //ESTE ES EL COMPONENTE DE MIS COMPRAS, CUANDO QUIERES VER TODAS LAS COMPRAS DEL USUARIO
 
 @Component({
@@ -10,31 +10,18 @@ import { MisComprasService } from 'src/app/services/mis-compras.service';
   styleUrls: ['./mis-compras.component.css']
 })
 export class MisComprasComponent {
-<<<<<<< HEAD
+
   public totalCompras: number = 0; //varible donde almacenaremos el total
   public misCompras: MisCompras[] = [];
   public emailUsuario: string = "null";
-=======
-  public totalCompras: number = 0; //variable donde almacenaremos el total
-  public misCompras: MisCompras[] = []; //almacenas todos los productos
-  public emailUsuario: string = "null"; //donde almacenas el usuario vacio
->>>>>>> ff90a6940405472fea974b1ce7d244dc78a633f7
 
   constructor(
     public misComprasService: MisComprasService, //almacenas todas las compras
     public usuarioService: UsuarioService){}
 
-<<<<<<< HEAD
 
-    //EJERCICIO 27 -->  La funcionalidad "Mis compras", solamente muestra la primera compra del usuario autenticado. (Sin borrar el resto)
-  ngOnInit(): void {
-    this.emailUsuario = this.usuarioService.obtenerEmailUsuarioLogeado(); //obtener usuario logeado
-
-    this.misComprasService.misComprasSeleccionadas$.subscribe(
-      compras => {
-=======
   ngOnInit(): void { //EJRCICIO 27
-    
+
     this.emailUsuario = this.usuarioService.obtenerEmailUsuarioLogeado(); //obtienes el email del usuario logeado
 
     this.misComprasService.misComprasSeleccionadas$.subscribe(
@@ -42,7 +29,6 @@ export class MisComprasComponent {
         this.misCompras = compras;
         //calculas el total de la compra
         this.totalCompras = 0;
->>>>>>> ff90a6940405472fea974b1ce7d244dc78a633f7
 
         this.misCompras = compras.slice(0, 1); //esto --> de todo el array de compras te muestra la primera de todas
         this.calcularTotalCompras(); //esto -->
@@ -56,7 +42,7 @@ export class MisComprasComponent {
   //metodo calcularTotal --> suma el total de la compra
   calcularTotalCompras(): void { //metodo que recalcula el total de las compras
     this.totalCompras = 0; //contador
-    this.misCompras.forEach(compra => { //recorres todas las compras 
+    this.misCompras.forEach(compra => { //recorres todas las compras
       this.totalCompras += Number(compra.total); //sumas el total de todas las compras
     });
   }
