399a92ba9245df4d3f0e67e5abf530f9a2d31b76
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-05-22 14:45:00 +0200

    C

--- server/controllers/carrito.controller.js
+++ server/controllers/carrito.controller.js
@@ -79,6 +79,26 @@ carritoController.agregarAlCarrito = async (req, res) => {
         //buscamos el carrito del usuario
         let carritoUsuario = await obtenerCarritoUsuario(usuarioActual);
 
+        
+
+
+        //NO METER MAS DE DOS PRODUCTOS EN EL CARRITO, (NO DE STOCK, OJO!) -- SOLO ES ESTE CODIGO
+        //calcular el total de productos que hay en el carritoç
+        let totalProductos = carritoUsuario.productos.reduce((sum, prod) => sum + prod.cantidad, 0);
+        if (totalProductos >= 2) {
+            return res.status({ status: 'No se pueden añadir más de dos productos al carrito' }); //RES STATUS SUPER IMPORTANTE SINO NO FUNCIONA
+        }
+        // FIN DEL CODIGO
+
+
+
+
+
+
+
+
+
+
         //buscamos la posicion del producto dentro del array
         const indexProducto = carritoUsuario.productos.findIndex(producto => producto.nombreProd === nombreProd);
 
