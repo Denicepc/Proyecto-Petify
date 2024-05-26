f95eeda18241db479de90905870dea2eaddd7929
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-05-26 14:03:57 +0200

    c

--- frontend/src/app/components/ej42/ej42.component.ts
+++ frontend/src/app/components/ej42/ej42.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MisComprasService } from 'src/app/services/mis-compras.service';
 import { MisCompras } from 'src/app/models/mis-compras';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-ej42',
@@ -9,23 +11,49 @@ import { MisCompras } from 'src/app/models/mis-compras';
 })
 export class Ej42Component{
 
-  public compras: MisCompras[] = [];
-  public maxPrecio: number = 0;
-  public nombreProd: string = "";
+  //variables que solo necesitas
+  public maxPrecio : number = 0;
+  public nombreProducto : string = "";
 
-  constructor(private misComprasService: MisComprasService) { }
+  constructor(public usuarioService: UsuarioService, public misComprasService: MisComprasService){
 
-  cargarDatos(): void {
-    this.misComprasService.obtenerCompras().subscribe((res: any) => {
-      this.compras = res;
-      this.compras.forEach(compra => {
-        compra.productos.forEach(producto => {
-          if (producto.precio > this.maxPrecio) {
-            this.maxPrecio = producto.precio;
-            this.nombreProd = producto.nombreProd;
-          }
-        });
-      });
-    });
+    let usuarios : Usuario[] = [];
+    let compras : MisCompras[] = [];
+    let precioMaximo : number = 0;
+    let nomProd : string = "";
+
+    this.usuarioService.getUsuarios().subscribe(
+      (res : any) =>{
+
+        usuarios = res;
+        for(let usu of usuarios){
+
+          this.misComprasService.obtenerComprasUsuario(usu.email).subscribe(
+            (res : any) =>{
+              compras = res;
+
+              compras.forEach( compra =>{
+                compra.productos.forEach(producto =>{
+                  if(producto.precio > precioMaximo){
+                    precioMaximo = producto.precio;
+                    nomProd = producto.nombreProd;
+                  }
+                })
+              })
+            }
+          )
+
+          setTimeout(() =>{
+            this.maxPrecio = precioMaximo;
+            this.nombreProducto = nomProd;
+          }, 200)
+
+        }
+
+      }
+    )
+
+
   }
+
 }
