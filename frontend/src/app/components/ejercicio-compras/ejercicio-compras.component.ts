e36d4cd21d10644ebd304a3dcd2496672669e139
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-05-27 11:16:41 +0200

    c

--- frontend/src/app/components/ejercicio-compras/ejercicio-compras.component.ts
+++ frontend/src/app/components/ejercicio-compras/ejercicio-compras.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MisComprasService } from 'src/app/services/mis-compras.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -7,25 +7,26 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   templateUrl: './ejercicio-compras.component.html',
   styleUrls: ['./ejercicio-compras.component.css']
 })
-export class EjercicioComprasComponent {
+export class EjercicioComprasComponent{
 
-  public email : string = "";
   public primerProducto : string = "";
   public ultimoProducto : string = "";
 
   constructor(public misComprasService: MisComprasService, public usuarioService: UsuarioService){}
 
-  ngOnInit(): void {
+  recogerProducto() {
     const emailUsuario = this.usuarioService.obtenerEmailUsuarioLogeado();
-    this.misComprasService.obtenerComprasUsuario(emailUsuario).subscribe(compras => {
+    this.misComprasService.obtenerComprasUsuario(emailUsuario).subscribe(
+     ( compra : any) =>{
 
-        const primeraCompra = compras[0];
-        const ultimaCompra = compras[compras.length - 1];
+      const primeraCompra = compra[0];
+      const ultimaCompra = compra[compra.length - 1];
 
+      this.primerProducto = primeraCompra.productos[0].nombreProd;
+      this.ultimoProducto = ultimaCompra.productos[ultimaCompra.productos.length - 1].nombreProd;
 
-        this.primerProducto = primeraCompra.productos[0].nombreProd;
-        this.ultimoProducto = ultimaCompra.productos[ultimaCompra.productos.length - 1].nombreProd;
-      }
+     }
+
     )}
   }
 
