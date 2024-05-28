11d6b9baa0e2d7f434dd0631402bdaf49f6067b2
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-05-28 14:25:30 +0200

    c

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/models/usuario';
 import { MisComprasService } from 'src/app/services/mis-compras.service';
 import { MisCompras } from 'src/app/models/mis-compras';
+import { CarritoService } from 'src/app/services/carrito.service';
 
 @Component({
   selector: 'app-login',
@@ -23,9 +24,8 @@ export class LoginComponent {
   public esAdmin: boolean = false;
   public misCompras: MisCompras[] = [];
 
-  constructor(public usuarioService: UsuarioService, 
-    public misComprasService: MisComprasService,
-    public formBuilder: FormBuilder){
+  //METES EL CARRITO SERVICE DENTRO
+  constructor(public usuarioService: UsuarioService, public misComprasService: MisComprasService,public carritoService: CarritoService ,public formBuilder: FormBuilder){
       this.enviarEmail = new EventEmitter();
   }
 
@@ -59,8 +59,8 @@ export class LoginComponent {
     }
 
     this.usuarioService.registrarUsuario(form.value)
-      .subscribe( 
-      (response: any) => {   
+      .subscribe(
+      (response: any) => {
         if(response.status === "Usuario registrado correctamente"){
           alert('Usuario registrado Correctamente');
 
@@ -116,7 +116,7 @@ export class LoginComponent {
         if(response.status == "Inicio de sesión correcto"){
           //si la solicitud es exitosa
           console.log('Inicio de sesión exitoso', response);
-  
+
           //funciones adicionales
           if (response.usuario) {
             this.usuario = response.usuario;
@@ -124,10 +124,22 @@ export class LoginComponent {
 
           //si se inicia sesion asociamos el id a su carrito
           this.enviarEmail.emit(this.usuario.email);
-            
+
             if(this.usuario.rol === "Administrador"){
               this.esAdmin = true;
             }
+
+
+            //EMPIEZAS EL CODIGO --------------------------------------------
+            this.carritoService.obtenerCarrito(this.usuario.email).subscribe(
+              (carrito : any) =>{
+                this.carritoService.actualizarCarritoSeleccionado(carrito);
+              }
+            )
+            //FINALIZAS EL CODIGO -------------------------------------------
+
+
+
           }
           this.mostrarInicio = true;
           this.colorIconoUsuario = "#58d156"; //color verde
@@ -189,14 +201,14 @@ export class LoginComponent {
             }
           );
         }
-      } 
+      }
     }
 
   }
 
   cerrarSesion(){
     this.haIniciado = false;
-    this.nombreUsuario = "Usuario sin identificar";  
+    this.nombreUsuario = "Usuario sin identificar";
     this.colorIconoUsuario = "black";
     this.usuarioService.usuarioSeleccionado = new Usuario(); // reseteamos el usuario
     this.usuario = new Usuario();
@@ -230,7 +242,7 @@ export class LoginComponent {
     }
 
     this.enviarEmail.emit("null");  //quitamos el id asociado al carrito
-    this.usuarioService.emailUsuarioLogeado = "null"; 
+    this.usuarioService.emailUsuarioLogeado = "null";
 
     //vaciamos mis compras
     this.misCompras = [];
--- server/controllers/usuario.controller.js
+++ server/controllers/usuario.controller.js
@@ -1,14 +1,64 @@
-const usuario = require('../models/usuario');
-const usuarioController = {};
+    const usuario = require('../models/usuario');
+    const carrito = require('../models/carrito'); //SUPER IMPORTANTE METER ESTA RUTA
+    const usuarioController = {};
+
+
+    //CREAS LOS PRODUCTOS QUE VAS A METER
+    const productosIniciales = [
+        {nombreProd: 'Indoor Cat', cantidad: 3, precio: 10, stock: 13},
+        {nombreProd: 'Bountifull catch', cantidad: 2, precio: 15, stock: 12},
+        {nombreProd: 'Wild Prairie', cantidad: 1, precio: 17, stock: 15}
+    ];
+
+
+    //METODO PARA CREAR UN CARRITO VACIO
+    const obtenerCarritoUsuario = async (emailUsuario) => {
+        const emailUsuarioFinal = emailUsuario ? emailUsuario : "null";
+        
+        let carritoActual = await carrito.findOne({ emailUsuario: emailUsuarioFinal });
+
+        //Si no encontramos un carrito para el usuario actual o usuario null, creamos uno nuevo
+        if (!carritoActual) {
+            carritoActual = new carrito({
+                emailUsuario: emailUsuarioFinal,
+                productos: [],
+                total: 0,
+            });
+            await carritoActual.save();
+        }
+        return carritoActual;
+    }
+
+
+    //INICIO DE SESION
+    usuarioController.iniciarSesion = async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const user = await usuario.findOne({ email });
+
+            if (!user || user.password !== password) { //si el usuario o la contraseña no coinciden entonces no existe
+                return  res.json({status: 'Datos incorrectos al iniciar sesión'});
+            }
+
+
+            //EMPEZAMOS EL CAMBIO DE CODIGO AQUI
+            let carritoUsuario = await obtenerCarritoUsuario(email);
+            if(carritoUsuario.productos.length === 0){ //si el carrito no tiene productos... que es el caso
+                productosIniciales.forEach(producto =>{
+                    carritoUsuario.productos.push(producto);
+                    carritoUsuario.total += producto.cantidad * producto.precio;
+                });
+                await carritoUsuario.save();
+            }
+            //FIN CAMBIO DE CODIGO
 
 
 
-
-
-
-
-
-
+            res.json({status: 'Inicio de sesión correcto', usuario: user});
+        } catch (error) {
+            res.json({status: 'Error al inciar sesión'});
+        }
+    };
 
 
 
@@ -106,21 +156,7 @@ usuarioController.registrarUsuario = async (req, res) => {
     }
 };
 
-//inicio de sesión
-usuarioController.iniciarSesion = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await usuario.findOne({ email });
 
-        if (!user || user.password !== password) { //si el usuario o la contraseña no coinciden entonces no existe
-            return  res.json({status: 'Datos incorrectos al iniciar sesión'});
-        }
-
-        res.json({status: 'Inicio de sesión correcto', usuario: user});
-    } catch (error) {
-        res.json({status: 'Error al inciar sesión'});
-    }
-};
 
 
 module.exports = usuarioController;
