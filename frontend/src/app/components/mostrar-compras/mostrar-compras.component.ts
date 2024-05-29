7969092bdc30dff9b49eb201512974fb1465604e
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-05-29 14:58:35 +0200

    c

---
+++ frontend/src/app/components/mostrar-compras/mostrar-compras.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-compras',
+  templateUrl: './mostrar-compras.component.html',
+  styleUrls: ['./mostrar-compras.component.css']
+})
+export class MostrarComprasComponent {
+
+}
