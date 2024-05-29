5be9fd8d6ad812129aad6d968ef54f0a871bb783
marioolopez <150073715+marioolopez@users.noreply.github.com>
2024-05-29 15:08:48 +0200

    c

--- frontend/src/app/components/mostrar-compras/mostrar-compras.component.ts
+++ frontend/src/app/components/mostrar-compras/mostrar-compras.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MisCompras } from 'src/app/models/mis-compras';
+import { MisComprasService } from 'src/app/services/mis-compras.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-mostrar-compras',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./mostrar-compras.component.css']
 })
 export class MostrarComprasComponent {
+
+  public primerProducto : string = "";
+  public segundoProducto : string = "";
+
+  public compras: MisCompras[]=[];
+  public email : string = "";
+
+  constructor(public misComprasService: MisComprasService, public usuarioService: UsuarioService){}
+
+  recogerCompras(){
+    this.email = this.usuarioService.emailUsuarioLogeado;
+    this.misComprasService.obtenerComprasUsuario(this.email).subscribe(
+      (res : any) => {
+        this.compras = res;
+
+        const primeraCompra = this.compras[0]; //primera compra
+        const ultimaCompra = this.compras[this.compras.length-1]; //ultima compra
+
+        this.primerProducto = primeraCompra.productos[0].nombreProd;
+        this.segundoProducto = ultimaCompra.productos[ultimaCompra.productos.length -1].nombreProd;
+
+        });
+  }
 
 }
